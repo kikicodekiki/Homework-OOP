a7ce1f15ba884a7eda2fc2cdbebeef90d0f1d5bb
Ekaterina Stoyanova <147726778+kikicodekiki@users.noreply.github.com>
2024-04-20 12:49:02 +0300

    Update ModifiableIntegersFunction.h - complete comparison functions

--- ModifiableIntegerFunction/ModifiableIntegersFunction.h
+++ ModifiableIntegerFunction/ModifiableIntegersFunction.h
@@ -8,25 +8,6 @@ int16_t dummyFunc (int16_t x) {
 int16_t (*myDummyFunc) (int16_t x) = dummyFunc;
 
 class ModifiableIntegersFunction {
-private:
-    int16_t (*ogFunc) (int16_t); // func pointer
-
-    static const int MAX_SIZE = UINT16_MAX + 1 ;
-
-    //this was the most memory & time
-    // efficient approach I could come up with for this task
-    int16_t** results; //can either hold the point or be nullptr; using uint16_t's range
-
-    bool isModified [MAX_SIZE]; //flag to check modification so as to not initialise the whole results array
-    bool isExcluded [MAX_SIZE]; //flag to check if it has been modified
-
-    //for big 4
-    void free();
-    void copyFrom (const ModifiableIntegersFunction& other);
-
-    bool isPointExcluded (int16_t input) const;
-    bool isPointNull ( int16_t input ) const;
-
 public:
 
     ModifiableIntegersFunction(int16_t(*myFunc)(int16_t));
@@ -42,14 +23,42 @@ public:
     void setResultForSpecificInput (int16_t input, int16_t wantedOutput);
     void excludePoint (int16_t input);
 
+    bool isPointExcluded (int16_t input) const;
+    bool isPointNull ( int16_t input ) const;
+
     int16_t operator() (int16_t x) const; //func call operator
 
     ModifiableIntegersFunction& operator+= (const ModifiableIntegersFunction& rhs);
     ModifiableIntegersFunction& operator-= (const ModifiableIntegersFunction& rhs);
 
-    friend ModifiableIntegersFunction operator* (const ModifiableIntegersFunction& f, const ModifiableIntegersFunction& g) ;
-    friend ModifiableIntegersFunction operator+ (const ModifiableIntegersFunction& lhs, const ModifiableIntegersFunction& rhs);
-    friend ModifiableIntegersFunction operator- (const ModifiableIntegersFunction& lhs, const ModifiableIntegersFunction& rhs);
+    static const int MAX_SIZE = UINT16_MAX + 1 ;
 
+private:
+    int16_t (*ogFunc) (int16_t); // func pointer
+
+    //this was the most memory & time
+    // efficient approach I could come up with for this task
+    int16_t** results; //can either hold the point or be nullptr; using uint16_t's range
+
+    bool isModified [MAX_SIZE]; //flag to check modification so as to not initialise the whole results array
+    bool isExcluded [MAX_SIZE]; //flag to check if it has been modified
+
+    //for big 4
+    void free();
+    void copyFrom (const ModifiableIntegersFunction& other);
+    //void moveFrom(&&)
 
 };
+
+ModifiableIntegersFunction operator* (const ModifiableIntegersFunction& f, const ModifiableIntegersFunction& g) ;
+ModifiableIntegersFunction operator+ (const ModifiableIntegersFunction& lhs, const ModifiableIntegersFunction& rhs);
+ModifiableIntegersFunction operator- (const ModifiableIntegersFunction& lhs, const ModifiableIntegersFunction& rhs);
+
+bool operator< (const ModifiableIntegersFunction& f, const ModifiableIntegersFunction& g) ;
+bool operator> (const ModifiableIntegersFunction& f, const ModifiableIntegersFunction& g) ;
+bool operator== (const ModifiableIntegersFunction& f, const ModifiableIntegersFunction& g) ;
+bool operator!= (const ModifiableIntegersFunction& f, const ModifiableIntegersFunction& g) ;
+bool operator<= (const ModifiableIntegersFunction& f, const ModifiableIntegersFunction& g) ;
+bool operator>= (const ModifiableIntegersFunction& f, const ModifiableIntegersFunction& g) ;
+
+
