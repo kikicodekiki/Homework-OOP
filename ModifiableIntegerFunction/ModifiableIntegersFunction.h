ede231c224f1d76b75296b9c17e117c8c8f45a95
Ekaterina Stoyanova <147726778+kikicodekiki@users.noreply.github.com>
2024-04-20 10:05:15 +0300

    Update ModifiableIntegersFunction.h

--- ModifiableIntegerFunction/ModifiableIntegersFunction.h
+++ ModifiableIntegerFunction/ModifiableIntegersFunction.h
@@ -1,27 +1,55 @@
 #pragma once
 #include <iostream>
 
+int16_t dummyFunc (int16_t x) {
+    return x;
+}
+
+int16_t (*myDummyFunc) (int16_t x) = dummyFunc;
+
 class ModifiableIntegersFunction {
+private:
     int16_t (*ogFunc) (int16_t); // func pointer
 
-    int16_t results [UINT16_MAX]; //to hold te modified results; using uint16_t's range
+    static const int MAX_SIZE = UINT16_MAX + 1 ;
 
-    bool isModified [UINT16_MAX]; //flag to check modifications
+    //this was the most memory & time
+    // efficient approach I could come up with for this task
+    int16_t** results; //can either hold the point or be nullptr; using uint16_t's range
 
-    bool isExcluded [UINT16_MAX];
+    bool isModified [MAX_SIZE]; //flag to check modification so as to not initialise the whole results array
+    bool isExcluded [MAX_SIZE]; //flag to check if it has been modified
 
+    //for big 4
+    void free();
+    void copyFrom (const ModifiableIntegersFunction& other);
+
+    bool isPointExcluded (int16_t input) const;
+    bool isPointNull ( int16_t input ) const;
 
 public:
+
     ModifiableIntegersFunction(int16_t(*myFunc)(int16_t));
-    ~ModifiableIntegersFunction();
+    //default using dummy func ptr
+    ModifiableIntegersFunction() : ModifiableIntegersFunction(myDummyFunc) {}
+
+    ~ModifiableIntegersFunction() noexcept;
+
+    ModifiableIntegersFunction (const ModifiableIntegersFunction& other);
+
+    ModifiableIntegersFunction& operator= (const ModifiableIntegersFunction& other);
 
     void setResultForSpecificInput (int16_t input, int16_t wantedOutput);
     void excludePoint (int16_t input);
 
     int16_t operator() (int16_t x) const; //func call operator
 
+    ModifiableIntegersFunction& operator+= (const ModifiableIntegersFunction& rhs);
+    ModifiableIntegersFunction& operator-= (const ModifiableIntegersFunction& rhs);
 
-
+    friend ModifiableIntegersFunction operator* (const ModifiableIntegersFunction& f, const ModifiableIntegersFunction& g) ;
+    friend ModifiableIntegersFunction operator+ (const ModifiableIntegersFunction& lhs, const ModifiableIntegersFunction& rhs);
+    friend ModifiableIntegersFunction operator- (const ModifiableIntegersFunction& lhs, const ModifiableIntegersFunction& rhs);
 
 
 };
