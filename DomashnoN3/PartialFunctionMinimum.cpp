d2f6d2b109cbb18f21646811deb452f38074eadd
Ekaterina Stoyanova <147726778+kikicodekiki@users.noreply.github.com>
2024-06-03 17:07:26 +0300

    Create PartialFunctionMinimum.cpp

---
+++ DomashnoN3/PartialFunctionMinimum.cpp
@@ -0,0 +1,39 @@
+#include "PartialFunctionMinimum.h"
+PartialFunctionMinimum ::PartialFunctionMinimum(const PartialFunctionContainer &funcs) : ContainerHolder(container)
+{}
+
+bool PartialFunctionMinimum::isDefined(int32_t point) const {
+    size_t size = container.getSize();
+
+    for (size_t i = 0; i < size; i++) {
+        if (!container[i] ->isDefined(point)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int32_t PartialFunctionMinimum::operator()(int32_t point) const {
+    if (!isDefined(point)) {
+        throw std::runtime_error("point not defined"); //maybe not the best exception but idk
+    }
+
+    size_t size = container.getSize();
+
+    int32_t minimum = container[0]->operator()(point);
+
+    for (size_t i = 0; i < size; i++) {
+
+        int32_t temp = container[i]->operator()(point);
+
+        if (temp < minimum) {
+            minimum = temp;
+        }
+    }
+
+    return minimum;
+}
+
+PartialFunction* PartialFunctionMinimum::clone() const {
+    return new PartialFunctionMinimum(*this);
+}
