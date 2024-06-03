74e2ea516a742f97accaeb97a116f103323fae43
Ekaterina Stoyanova <147726778+kikicodekiki@users.noreply.github.com>
2024-06-03 17:08:37 +0300

    Create SecondCriteria.h

---
+++ DomashnoN3/SecondCriteria.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream>
+#include "Pair.h"
+#include "Criteria.h"
+
+class SecondCriteria: public Criteria { // T==1
+public:
+    SecondCriteria(int32_t* args, int16_t N);
+    Pair<bool,int32_t> operator()(int32_t point) const override;
+
+private:
+    int pointIndex(int32_t point) const override;
+};
