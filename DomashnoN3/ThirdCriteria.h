6f1be8020cdc7dd43b2ca9ea00f3eb3fe21dc4e3
Ekaterina Stoyanova <147726778+kikicodekiki@users.noreply.github.com>
2024-06-03 17:11:18 +0300

    Create ThirdCriteria.h

---
+++ DomashnoN3/ThirdCriteria.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+#include "Pair.h"
+#include "Criteria.h"
+
+
+class ThirdCriteria : public Criteria { //T == 2
+public:
+    ThirdCriteria(int32_t* args, int16_t N);
+    Pair<bool,int32_t> operator() (int32_t point) const override;
+
+private:
+    int pointIndex (int32_t point) const override;
+};
