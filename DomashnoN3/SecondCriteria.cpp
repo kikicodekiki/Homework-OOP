5d27682a4c838bf11cbff91ba67f0064aa66f939
Ekaterina Stoyanova <147726778+kikicodekiki@users.noreply.github.com>
2024-06-04 19:19:52 +0300

    Update SecondCriteria.cpp

--- DomashnoN3/SecondCriteria.cpp
+++ DomashnoN3/SecondCriteria.cpp
@@ -1,12 +1,4 @@
 #include "SecondCriteria.h"
-int SecondCriteria::pointIndex(int32_t point) const {
-    for (size_t i = 0; i < N; i++) {
-        if (point == arguments[i]) {
-            return i;
-        }
-    }
-    return -1;
-}
 
 Pair<bool,int32_t> SecondCriteria:: operator()(int32_t point) const {
     static int invalid = 0;
