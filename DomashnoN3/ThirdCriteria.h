1654162d0554065c28aa2e0f2e9a04c877269f9d
Ekaterina Stoyanova <147726778+kikicodekiki@users.noreply.github.com>
2024-06-04 19:20:24 +0300

    Update ThirdCriteria.h

--- DomashnoN3/ThirdCriteria.h
+++ DomashnoN3/ThirdCriteria.h
@@ -8,7 +8,4 @@ class ThirdCriteria : public Criteria { //T == 2
 public:
     ThirdCriteria(int32_t* args, int16_t N);
     Pair<bool,int32_t> operator() (int32_t point) const override;
-
-private:
-    int pointIndex (int32_t point) const override;
 };
