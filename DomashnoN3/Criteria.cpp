973a199b0999a484e31f107af7bb8ecd95ddde9d
Ekaterina Stoyanova <147726778+kikicodekiki@users.noreply.github.com>
2024-06-04 19:13:38 +0300

    Update Criteria.cpp

--- DomashnoN3/Criteria.cpp
+++ DomashnoN3/Criteria.cpp
@@ -2,7 +2,19 @@
 Criteria::Criteria(int32_t *newArgs, int16_t N) {
     this -> N = N;
 
-    for (size_t i = 0; i < MAX_SIZE; i++) {
+    for (size_t i = 0; i < N; i++) {
         arguments[i] = newArgs[i];
     }
+}
+
+
+int Criteria::getIndex(int32_t point) const {
+    static int invalidIndex = -1;
+
+    for (size_t i = 0; i < N; i++) {
+        if (point == arguments[i]) {
+            return i;
+        }
+    }
+    return invalidIndex;
 }
